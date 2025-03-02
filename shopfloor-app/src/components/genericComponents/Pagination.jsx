4b35b52adc7e7bf538fa6722e64288d43605573a
Robin Ledoux <robin.ledoux@student.hogent.be>
2025-03-02 15:45:01 +0100

    Small change to pagination UI

--- shopfloor-app/src/components/genericComponents/Pagination.jsx
+++ shopfloor-app/src/components/genericComponents/Pagination.jsx
@@ -1,3 +1,6 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+import { FaArrowLeftLong } from "react-icons/fa6";
+
 export function Pagination({currentPage, setCurrentPage, data, loading}){
 
     const handlePageChange = (newPage) => {
@@ -8,18 +11,20 @@ export function Pagination({currentPage, setCurrentPage, data, loading}){
 
     return (
         <div className="flex justify-center text-center gap-2">
-            <button className="transition-all font-bold hover:scale-110 hover:cursor-pointer"
+            <button className="flex items-center gap-1 transition-all font-bold hover:scale-110 hover:cursor-pointer"
             onClick={() => handlePageChange(currentPage - 1)}
             disabled={currentPage === 1 || loading}
             >
-            Prev
+                <FaArrowLeftLong/>
+                Prev
             </button>
             <span className="font-semibold">Page {currentPage}</span>
-            <button className="transition-all font-bold hover:scale-110 hover:cursor-pointer"
+            <button className="flex items-center gap-1 transition-all font-bold hover:scale-110 hover:cursor-pointer"
             onClick={() => handlePageChange(currentPage + 1)}
             disabled={loading}
             >
-            Next
+                Next
+                <FaArrowRightLong/>
             </button>
         </div>
     )
--- shopfloor-app/src/components/genericComponents/TableRow.jsx
+++ shopfloor-app/src/components/genericComponents/TableRow.jsx
@@ -7,7 +7,7 @@ const TableRow = ({ data, columns, onShow }) => {
         </td>
       ))}
       {onShow && (
-        <td className="border border-gray-300 px-4 md:py-2 text-center">
+        <td className="border border-gray-300 md:px-4 md:py-2 text-center max-sm:px-0 max-sm:p-0">
           <button
             className="bg-blue-500 text-white px-3 md:py-1 rounded hover:bg-blue-600 transition"
             onClick={() => onShow(data.id)}
