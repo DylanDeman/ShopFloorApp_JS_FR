312a73fbc4e4d1e7a587034c64ace15e9ef269d3
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-03-05 09:55:32 +0100

    Now fetches the kpi's and passes them to the dropdown
    Functionality of adding a kpi to a dashboard not yet implemented

--- shopfloor-app/src/pages/Dashboard.jsx
+++ shopfloor-app/src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import Dropdown from '../components/genericComponents/Dropdown';
 import TileList from '../components/KPI Tiles/TileList';
 import useSWR from 'swr';
-import { getAll } from '../api';
+import { getAll, save } from '../api';
 import AsyncData from '../components/AsyncData';
+import useSWRMutation from 'swr/mutation';
 
 const Dashboard = () => {
 
@@ -10,17 +11,38 @@ const Dashboard = () => {
     data: dashboards = [],
     loading,
     error,
-  } = useSWR('dashboards', getAll);
+  } = useSWR('dashboard', getAll);
 
-  console.log(dashboards);
+  const {
+    data: kpis = [],
+    loadingkpi,
+    errorkpi,
+  } = useSWR('kpi', getAll);
+
+  const { trigger: addKPIToDashboard, error: addKPIError } = useSWRMutation(
+    'dashboard',
+    (url, { arg }) => save(url, { arg }),
+  );
+
+  const addKPI = (kpi_id) => {
+    addKPIToDashboard({ id: 1, kpi_id });
+    if (addKPIError) {
+      console.error(addKPIError);
+    }
+  };
+
+  console.log(kpis);
 
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-6">Dashboard Pagina</h1>
-      <Dropdown
-        label={'+ Voeg een nieuwe KPI toe'}
-        options={['optie 1', 'optie 2', 'optie 3']}
-      />
+      <AsyncData loading={loadingkpi} error={errorkpi}>
+        <Dropdown
+          label={'+ Voeg een nieuwe KPI toe'}
+          options={kpis}
+          onSelect={addKPI}
+        />
+      </AsyncData>
       <AsyncData loading={loading} error={error}>
         <TileList tiles={dashboards} />
       </AsyncData>
