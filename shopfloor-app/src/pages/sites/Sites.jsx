d7591df0c000351a71eb8145d510c28d799bc8f0
SijadWalipoor <Sijad.walipoor@student.hogent.be>
2025-03-08 01:06:19 +0100

    fixed small bugfix with position of add site button

--- shopfloor-app/src/pages/sites/Sites.jsx
+++ shopfloor-app/src/pages/sites/Sites.jsx
@@ -10,11 +10,10 @@ const Sites = () => {
   
   return (
     <>
-      <div className="flex justify-between items-center">
-        <h1 className="text-4xl font-semibold mb-6 mt-10"> 
+      <div className="flex justify-between items-center mb-6 mt-10">
+        <h1 className="text-4xl font-semibold"> 
           Overzicht sites 
         </h1>
-        
         <button 
           className="bg-red-500 hover:cursor-pointer hover:bg-red-700 
           text-white font-bold py-2 px-4 
