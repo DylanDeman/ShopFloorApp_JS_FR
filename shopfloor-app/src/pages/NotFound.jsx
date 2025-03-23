e2df109ec23ee7b0fcb2041eb98c67677152e7e9
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-03-23 07:34:05 +0100

    Added simple notfound page

--- shopfloor-app/src/pages/NotFound.jsx
+++ shopfloor-app/src/pages/NotFound.jsx
@@ -1,6 +1,15 @@
 const NotFound = () => {
   return (
-    <h1>Deze pagina bestaat niet!</h1>
+    <div className="flex flex-col items-center justify-center mt-30 text-center p-6">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Pagina niet gevonden</h1>
+      <p className="text-lg text-gray-600 mb-6">Oeps! De pagina die je zoekt bestaat niet.</p>
+      <a
+        href="/"
+        className="px-5 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition"
+      >
+        Ga terug naar home
+      </a>
+    </div>
   );
 };
 
